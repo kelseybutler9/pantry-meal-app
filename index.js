883e0850ae308cd9ac3c19c411d3c400345dc041
Kelsey Butler <kelseybutler.11@gmail.com>
2017-09-22 20:19:53 -0500

    add function to get ingredient suggestions

---
+++ index.js
@@ -0,0 +1,44 @@
+const recipe_search_URL = 'https://spoonacular-recipe-food-nutrition-v1.p.mashape.com/recipes/findByIngredients'
+const ingredient_search_URL = 'https://spoonacular-recipe-food-nutrition-v1.p.mashape.com/food/ingredients/autocomplete';
+const recipe_search_KEY = '8P5jfI6bzJmshGoHP4ULJXria9Lmp17aDJYjsnaBmFSetYNBoE'; 
+const recipe_search_APIID = '340adb33'; 
+
+function userSubmitsIngredients() {
+	$('.js-search-form').on('submit', function(event) {
+		event.preventDefault();
+		let ingredient = $('input[name=ingredient-search]').val();
+		getIngredientData(ingredient, displayIngredientList);
+		$('input[name=ingredient-search]').val('');
+		$('.js-suggestion-list').prop('hidden', false);
+	});
+}
+
+function getIngredientData (ingredient, callback) {
+	const settings = {
+    	url: ingredient_search_URL,
+    	headers: {
+        	'X-Mashape-Key': recipe_search_KEY,
+        	'Content-Type':'application/x-www-form-urlencode',
+        	'Accept':'application/json'
+    	},
+    	method: 'GET',
+    	dataType: 'json',
+    	data: {
+			intolerances: null,
+			metaInformation: false,
+			number: 2,
+			query: `${ingredient}`
+		},
+    	success: callback
+    };
+
+	$.ajax(settings).fail(showError());
+}	
+
+function displayIngredientList(data) {
+	console.log(data);
+}
+
+
+
+$(userSubmitsIngredients);
